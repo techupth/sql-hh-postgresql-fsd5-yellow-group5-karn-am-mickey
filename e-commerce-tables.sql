e96c3571e39eee0bb09eb1b5c4d9b75f60efb79d
acorriaz <mikey.swx@gmail.com>
2024-01-23 13:35:49 +0700

    Add ex

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,28 @@
 -- Start writing code here
+CREATE TABLE users (
+    user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR ( 255 ) UNIQUE NOT NULL,
+    email VARCHAR ( 255 ) UNIQUE NOT NULL,
+    password VARCHAR ( 255 ) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+)
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ) NOT NULL,
+    description TEXT,
+    price NUMERIC ( 10, 2 ) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+)   
+
+CREATE TABLE categories (
+    category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 255 ) UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+)
+
+CREATE TABLE products_categories (
+    product_category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    product_id INT REFERENCES products(product_id) ON DELETE CASCADE,
+    category_id INT REFERENCES categories(category_id) ON DELETE CASCADE
+)
