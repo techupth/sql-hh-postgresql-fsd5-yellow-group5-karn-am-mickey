cd0c4c070faa9433b262188d2b20ee86e3f21cdc
noppadon <noppadonsangthongw@gmail.com>
2024-01-24 23:59:32 +0700

    assign postgreSQL

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,28 @@
 -- Start writing code here
+CREATE TABLE users (
+	user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	username VARCHAR ( 255 ) UNIQUE NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	password VARCHAR ( 255 ) NOT NULL,
+	created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE products (
+	product_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR ( 255 ) NOT NULL,
+	description TEXT NULL,
+	price NUMERIC ( 10,2 ) NOT NULL,
+	created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE categories (
+	category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_at TIMESTAMPTZ UNIQUE NOT NULL
+);
+
+CREATE TABLE product_categories (
+	product_category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	product_id INT REFERENCES products(product_id) ON DELETE CASCADE UNIQUE,
+	category_id INT REFERENCES categories(category_id) ON DELETE CASCADE UNIQUE
+);
